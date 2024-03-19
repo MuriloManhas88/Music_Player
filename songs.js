f2b0998f6dc9d53aecacfc2e68e3989039da28fe
Murilo Manhas <murilomanhas588@gmail.com>
2024-03-19 11:12:41 -0300

    Music Player js

---
+++ songs.js
@@ -0,0 +1,20 @@
+export default [
+    {
+      src: "./src/Charlie Brown Jr - Lugar ao Sol.mp3",
+      name: "Lugar Ao Sol - Chorão",
+    },
+    {
+      src: "./src/Charlie Brown Jr - Hoje Eu Acordei Feliz.mp3",
+      name: "Hoje eu acordoei feliz - Chorão",
+    },
+    {
+      src: "./src/Cold Play - Viva La Vida.mp3",
+      name: "Viva La Vida - Coldplay",
+    },
+   
+    {
+        src: "./src/Menor MC - Tipo Gringa feat DJ Matt-D.mp3",
+        name: "Tipo Gringa- Menor Mc",
+      },
+  ];
+  
